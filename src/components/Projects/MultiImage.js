2f190eab6b62bb5fa1803c5ece123893ea97bfca
tucker reisig <tuckerreisig@Tuckers-MacBook-Pro.local>
2020-12-05 16:42:51 -0500

    800px done

--- src/components/Projects/MultiImage.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function MutliImage() {
-  return <div className="proj-multi-div"></div>;
-}
-
-export default MutliImage;
--- src/components/Summer/LeftText.js
+++ src/components/Summer/LeftText.js
@@ -2,15 +2,17 @@ import React from "react";
 
 function LeftText() {
   return (
-    <div className="left-text-div">
-      <span className="subtitle"> UNC Summer of Code</span>
-      <h4 className="title"> UNC Computer Science Resume Database </h4>
-      <p className="p-description">
-        The UNC CS Resume Database is used by the Department of Computer Science
-        and their corporate partners, allowing recruiters to easily query
-        students that match a given job requirement, making the recruiting
-        process easier in a post-Covid19 world.
-      </p>
+    <div className="wrapper-div">
+      <div className="left-text-div">
+        <span className="subtitle"> UNC Summer of Code</span>
+        <h4 className="title"> UNC Computer Science Resume Database </h4>
+        <p className="p-description">
+          The UNC CS Resume Database is used by the Department of Computer
+          Science and their corporate partners, allowing recruiters to easily
+          query students that match a given job requirement, making the
+          recruiting process easier in a post-Covid19 world.
+        </p>
+      </div>
     </div>
   );
 }
