ee3ee1c93dddead91b006240c7858e2da3bd9151
tucker reisig <tuckerreisig@Tuckers-MacBook-Pro.local>
2021-01-04 17:11:33 -0500

    removed old code

--- src/components/Home/WelcomeText.js
+++ src/components/Home/WelcomeText.js
@@ -3,18 +3,12 @@ import SvgDiv from "./SvgDiv";
 import Arrow from "./Arrow";
 
 function WelcomeText() {
-  //       ⬇️
   return (
     <div className="welcome-div">
       <div className="left-home-text">
-        {/* <h1 className="welcome-text">
-          {" "}
-          UNC Chapel Hill Computer Science Senior, Full Stack Developer{" "}
-        </h1> */}
         <h1 className="welcome-text"> UNC Chapel Hill</h1>
         <h1 className="welcome-text wide-h1"> Comp Sci Senior, </h1>
         <h1 className="welcome-text"> Full Stack Developer </h1>
-
         <div className="meet-span">
           <a href="#summer" className="name-anchor">
             <Arrow /> Meet Tucker Reisig{" "}
